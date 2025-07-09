2b3c1b0e5fd7e598f898160f82dd07d6285e723d
Mia <snubs_cicadas_3c@icloud.com>
2025-07-09 23:57:13 +0200

    fix(audiomonitor): resolve objc compiler errors

--- Thock/Services/AudioMonitor.swift
+++ Thock/Services/AudioMonitor.swift
@@ -3,38 +3,12 @@ import Foundation
 import ScriptingBridge
 import os.log
 
-@objc public enum MusicPlayerState: RawRepresentable, Equatable {
-    case stopped
-    case playing
-    case paused
-    case fastForwarding
-    case rewinding
-    case unknown(AEKeyword)
-
-    public typealias RawValue = AEKeyword
-
-    public init(rawValue: AEKeyword) {
-        switch rawValue {
-        case 0x6b505353: self = .stopped // 'kPSS'
-        case 0x6b505350: self = .playing // 'kPSP'
-        case 0x6b505370: self = .paused // 'kPSp'
-        case 0x6b505346: self = .fastForwarding // 'kPSF'
-        case 0x6b505352: self = .rewinding // 'kPSR'
-        default:
-            self = .unknown(rawValue)
-        }
-    }
-
-    public var rawValue: AEKeyword {
-        switch self {
-        case .stopped: return 0x6b505353
-        case .playing: return 0x6b505350
-        case .paused: return 0x6b505370
-        case .fastForwarding: return 0x6b505346
-        case .rewinding: return 0x6b505352
-        case .unknown(let val): return val
-        }
-    }
+@objc public enum MusicPlayerState: AEKeyword {
+    case stopped = 0x6b505353        // 'kPSS'
+    case playing = 0x6b505350        // 'kPSP'
+    case paused = 0x6b505370         // 'kPSp'
+    case fastForwarding = 0x6b505346 // 'kPSF'
+    case rewinding = 0x6b505352      // 'kPSR'
 }
 
 @objc public protocol MusicApplication {
@@ -52,8 +26,8 @@ class AudioMonitor {
 
     private init() {
         os_log("Initializing AudioMonitor.", log: self.logger, type: .info)
-        self.musicApp = SBApplication(bundleIdentifier: "com.apple.Music")
-        if self.musicApp == nil {
+        musicApp = SBApplication(bundleIdentifier: "com.apple.Music")
+        if musicApp == nil {
             os_log("Failed to get SBApplication instance for com.apple.Music.", log: self.logger, type: .error)
         }
     }
@@ -62,7 +36,7 @@ class AudioMonitor {
         os_log("Checking if music is playing...", log: self.logger, type: .debug)
 
         guard let musicApp = self.musicApp else {
-            os_log("Music application instance is nil.", log: aelf.logger, type: .error)
+            os_log("Music application instance is nil.", log: logger, type: .error)
             return false
         }
         
@@ -72,16 +46,12 @@ class AudioMonitor {
         }
 
         os_log("Music.app is running. Querying player state.", log: self.logger, type: .debug)
-        let currentState = musicApp.playerState
         
-        if let state = currentState {
-            if case .unknown(let rawValue) = state {
-                os_log("Received unknown player state with rawValue: %d", log: self.logger, type: .error, rawValue)
-            }
+        if let state = musicApp.playerState {
             os_log("Successfully retrieved player state: %{public}@", log: self.logger, type: .info, String(describing: state))
             return state == .playing
         } else {
-            os_log("Failed to retrieve player state. It returned nil. This is likely a permissions issue.", log: self.logger, type: .error)
+            os_log("Failed to retrieve a valid player state. It might be an unknown state or a permissions issue.", log: self.logger, type: .error)
             os_log("Please ensure Thock has Automation permissions for Music.app in System Settings > Privacy & Security > Automation.", log: self.logger, type: .error)
             return false
         }
